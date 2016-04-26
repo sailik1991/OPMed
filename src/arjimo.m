ed9aa0567aea65bcfd9462374b897da6dbac13c9
Sailik Sengupta <ssengu15@asu.edu>
2016-04-26 12:04:08 -0700

    Code for Barrier Method using Arjimo's backtracking line search for optimization problems with only inequality constrains

--- src/arjimo.m
+++ src/arjimo.m
@@ -2,17 +2,24 @@
 % @Inputs
 %   f(x)
 %   g(x) = f'(x)
+%   x_0 point for calculating descent
 %   symbols = list of symbols to represent f(x) in abstract format
-%   values = values to be assigned to the symbols
+%   values = values to be assigned to the symbols (other than 'x_0')
 %   dir = Descent direction
 %   a, b = Parameters for Arjimo's backtracking line search
+%
+%   @Returns
+%       't' value for step size
 function a_x = arjimo(f, g, x_0, symbols, other_values, dir, a, b)
     t = 1;
-    f_x = eval(subs(f, symbols, [x_0 other_values]));
+    val = [x_0 other_values];
+    f_x = eval(subs(f, symbols, val));
     while 1
         x_new = x_0 + t * transpose(dir);
-        f_x_new = eval(subs(f, symbols, [x_new other_values]));
-        chng = a*t*transpose(eval(subs(g, symbols, [x_0 other_values]))) * dir;
+        val = [x_new other_values];
+        f_x_new = eval(subs(f, symbols, val));
+        val = [x_0 other_values];
+        chng = a*t*transpose(eval(subs(g, symbols, val))) * dir;
         if isreal(f_x_new) == 0
             t = b * t;
             continue
